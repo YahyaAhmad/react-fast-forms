bb8e8eb78615d55b1b9a651af245b2b43ff8a9d2
Yahya Ahmad <yahya@sprintive.com>
2020-01-31 04:56:46 +0200

    Use a better example

--- public/index.js
+++ public/index.js
@@ -5,24 +5,8 @@ import { Field, Form, Submit } from "index";
 const FormExample = () => {
   return (
     <Form validateOnChange>
-      <Field
-        required
-        label="Test Field"
-        validators={{
-          pattern: /.+?\@\w+\.\w+/,
-          minLength: 12
-        }}
-        errorMessages={{
-          pattern: "This email is invalid",
-          minLength: "Value should be more than 11"
-        }}
-        component={"text"}
-        name="someName"
-      />
-      <section style={{ background: "red" }}>
-        <div>Please, put a number</div>
-        <Field required component="number" name="myNumber" />
-      </section>
+      <Field label="Title" component="text" name="title" required />
+      <Field label="Sur.Name" component="text" name="surName" />
       <Submit label="Submit" />
     </Form>
   );
