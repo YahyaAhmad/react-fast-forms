ce9dd68731aeaa9bfbc3930f88065f39475c42a7
Yahya Ahmad <yahya@sprintive.com>
2020-02-01 23:06:15 +0200

    Build

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,16 @@
+var path = require("path");
+
 module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    libraryTarget: "commonjs2"
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: ["node_modules", "src", "src/components", "src/utilities"]
+  },
   module: {
     rules: [
       {
