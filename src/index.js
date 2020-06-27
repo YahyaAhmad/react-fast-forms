f76fac82e210f557b7a99cd0e08b9e9cec0a5fa0
Yahya Al Hamad <yahya@sprintive.com>
2020-06-27 18:09:21 +0300

    Use data hook

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import Form from "Form";
 import Field from "components/Field";
 import Submit from "components/Submit";
 import Container from "components/Container";
-import { useField } from "utilities/hooks";
-export { Form, Submit, Field, Container, useField };
+import { useField, useData } from "utilities/hooks";
+export { Form, Submit, Field, Container, useField, useData };
--- src/utilities/hooks.js
+++ src/utilities/hooks.js
@@ -1,7 +1,13 @@
 import { useContext } from "react";
 import { FieldContext } from "components/Field";
+import { FormContext } from "Form";
 
 export const useField = () => {
   const { onChange, value, name, setError } = useContext(FieldContext);
   return { onChange, value, name, setError };
+};
+
+export const useData = () => {
+  const { data } = useContext(FormContext);
+  return data;
 };
